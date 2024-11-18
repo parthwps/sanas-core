5d4a821807e8ed3afe056095e27fee5c06abaef8
Prem Acharya <premcredsoft@gmail.com>
2024-11-18 20:47:56 +0530

    Update login-function.php

--- core/login-function.php
+++ core/login-function.php
@@ -698,10 +698,10 @@ if (!function_exists('sanas_guest_info')) {
             
             // prepare email headers
             $headers = array('Content-Type: text/html; charset=UTF-8');
-            wp_send_json_success(array(
-                'message' => 'Guest inserted successfully. '.$guest_id.'.'.$guestEmail.'.'.$subject.'.'.$body.'.', 
-                'guest_id' => $guest_id // Include the guest ID in the response
-            ));
+            // wp_send_json_success(array(
+            //     'message' => 'Guest inserted successfully. '.$guest_id.'.'.$guestEmail.'.'.$subject.'.'.$body.'.', 
+            //     'guest_id' => $guest_id // Include the guest ID in the response
+            // ));
 
             // send the email
             if(wp_mail($guestEmail, $subject, $body, $headers)){
