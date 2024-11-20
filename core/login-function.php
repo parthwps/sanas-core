74aeb740c1d60532e93a859f7833194000094d7b
Prem Acharya <premcredsoft@gmail.com>
2024-11-20 18:42:47 +0530

    Update login-function.php

--- core/login-function.php
+++ core/login-function.php
@@ -1027,7 +1027,7 @@ function sanas_guest_invitation_response_mail($guest_email, $status, $kidsguest,
     
     $body = str_replace(
         array('%%guestname', '%%gueststatus', '%%guestkids', '%%guestadult', '%%eventimg', '%%eventname', '%%eventdate', '%%eventtime', '%%eventlocation', '%%invitelink', '%%eventhost'), 
-        array($guest_name, $status, $kidsguest, $adultguest, $event_image, $event_name, $event_date, $event_date, $event_location, $invite_link, $event_host),
+        array($guest_name, $status, $kidsguest, $adultguest, $event_image, $event_name, $event_date, $event_time, $event_location, $invite_link, $event_host),
         $body
     );
 
