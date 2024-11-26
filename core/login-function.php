56eedb9b0e1bfc3cda91425364091f925449cddb
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-11-26 12:23:28 +0530

    Update login-function.php

--- core/login-function.php
+++ core/login-function.php
@@ -1002,7 +1002,7 @@ function sanas_guest_invitation_response() {
     );
 
     if($prestatus != $status || $prekidsguest != $kidsguest || $preadultguest != $adultguest){
-        echo sanas_guest_invitation_response_mail($guest_email, $status, $kidsguest, $prekidsguest, $adultguest, $preadultguest, $event_image_url, $guest_name, $event_name, $event_date, $event_time_line, $event_location, $invite_link, $event_host);
+        echo sanas_guest_invitation_response_mail($guest_email, $status, $prestatus, $kidsguest, $prekidsguest, $adultguest, $preadultguest, $event_image_url, $guest_name, $event_name, $event_date, $event_time_line, $event_location, $invite_link, $event_host);
     }
     echo '<div class="alert alert-success pop-btn-div" role="alert">' . esc_html__('Guest Submitted Response Successfully.', 'sanas') . '</div>';
 
@@ -1010,10 +1010,10 @@ function sanas_guest_invitation_response() {
 }
 
 //send mail to guest
-function sanas_guest_invitation_response_mail($guest_email, $status, $kidsguest, $prekidsguest, $adultguest, $preadultguest, $event_image, $guest_name, $event_name, $event_date, $event_time_line, $event_location, $invite_link, $event_host) {
+function sanas_guest_invitation_response_mail($guest_email, $status, $prestatus, $kidsguest, $prekidsguest, $adultguest, $preadultguest, $event_image, $guest_name, $event_name, $event_date, $event_time_line, $event_location, $invite_link, $event_host) {
     
     // if update the $kidsguest, $adultguest then send update mail
-    if ($kidsguest != $prekidsguest || $adultguest != $preadultguest) {
+    if ($kidsguest != $prekidsguest || $adultguest != $preadultguest || $status != $prestatus) {
         // if maybe
         if ($status == 'May Be') {
             $subject = sanas_options('sanas_guest_update_maybe_subject');
