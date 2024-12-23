22ac7055400e9b1e8753787708aa9e33eb9ea403
parthwps <parth@netdevs.net>
2024-12-23 09:25:45 +0530

    Update search-widget.php

--- elementor/widgets/search-widget.php
+++ elementor/widgets/search-widget.php
@@ -38,7 +38,7 @@ class Sanas_Search_Widget extends Widget_Base {
     protected function render() {
         $settings = $this->get_settings_for_display(); ?>
         <form class="search-form text-center" method="get" action="<?php echo esc_url(home_url( '/' )); ?>">
-            <input type="search" id="search" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_html($settings['sanas_search_text']); ?>">
+            <input type="search" id="search" name="s" autocomplete="off" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_html($settings['sanas_search_text']); ?>">
             <ul id="suggestionlist"></ul>
             <button class="search-btn" type="button"><i class="icon-Search"></i></button>
         </form>
