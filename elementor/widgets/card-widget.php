04fac0e061dca4bf35504f0b8e466afc8c338763
Prem Acharya <premcredsoft@gmail.com>
2024-12-24 19:59:51 +0530

    Update card-widget.php

--- elementor/widgets/card-widget.php
+++ elementor/widgets/card-widget.php
@@ -130,14 +130,13 @@ if ($query->have_posts()) :
 
             ?>
 
-            <div class="card-box col-lg-3 col-md-4 col-sm-6 card-preview" data-card-id="<?php echo get_the_ID(); ?>">
+            <div class="card-box col-lg-3 col-md-4 col-sm-6 card-preview" data-card-id="<?php echo get_the_ID(); ?>" data-bg-color="<?php echo isset($bg_color) ? $bg_color : ''; ?>">
                 <div class="inner-box">
                     <a 
                     href="javascript:void(0)" 
                     class="<?php echo is_user_logged_in() ? '' : 'login-in sanas-login-popup'; ?> flip-container" 
                     <?php echo !is_user_logged_in() ? 'data-href="' . esc_url($dashboardURL) . '" data-card-id="' . get_the_ID() . '"' : ''; ?> 
                     <?php echo isset($bg_color) ? $bg_color : ''; ?>
-                    data-bg-color="<?php echo isset($bg_color) ? $bg_color : ''; ?>"
                     >
                         <div class="flipper">
                             <div class="front">
