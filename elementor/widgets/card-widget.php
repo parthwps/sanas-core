158850ae0323052a2835a5e59a96fe5edda254fc
Prem Acharya <166224472+premacharya15@users.noreply.github.com>
2024-12-28 15:59:57 +0530

    Added card-preview-data classe

--- elementor/widgets/card-widget.php
+++ elementor/widgets/card-widget.php
@@ -131,7 +131,7 @@ if ($query->have_posts()) :
 
             ?>
 
-            <div class="card-box col-lg-3 col-md-4 col-sm-6" data-card-id="<?php echo get_the_ID(); ?>" data-bgcolor-code="<?php echo isset($bg_color_code) ? $bg_color_code : ''; ?>" data-bg-color="<?php echo isset($bg_color) ? $bg_color : ''; ?>"
+            <div class="card-box col-lg-3 col-md-4 col-sm-6  card-preview-data" data-card-id="<?php echo get_the_ID(); ?>" data-bgcolor-code="<?php echo isset($bg_color_code) ? $bg_color_code : ''; ?>" data-bg-color="<?php echo isset($bg_color) ? $bg_color : ''; ?>"
                     data-front-img="<?php echo esc_url($sanas_portfolio_meta['sanas_upload_front_Image']['url']); ?>" data-href="<?php echo esc_url($dashboardURL); ?>"
                     data-back-img="<?php echo esc_url($sanas_portfolio_meta['sanas_upload_back_Image']['url']); ?>" data-card-title="<?php echo esc_html(get_the_title()); ?>">
                 <div class="inner-box">
